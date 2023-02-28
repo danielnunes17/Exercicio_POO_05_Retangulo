efc569f2ac75f47c10c7280dc3c4a28e5a7411ce
Daniel Stephanio Nunes <dti584@localiza.com>
2023-02-28 11:10:26 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/*Fazer um programa para ler os valores da largura e altura de um retângulo.
+ Em seguida, mostrar na tela o valor de sua área, perímetro e diagonal.
+Exemplo:
+Entre a largura e altura do retângulo:
+3.00
+4.00
+AREA = 12.00
+PERÍMETRO = 14.00
+DIAGONAL = 5.00*/
+
+using Exercicio_POO_05_Retangulo;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Retangulo retangulo = new Retangulo();
+
+        Console.WriteLine("Entre com a largura do retângulo: ");
+        retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Entre com altura do retângulo: ");
+        retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("AREA = " + retangulo.CalcularArea().ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("PERÍMETRO = " + retangulo.CalcularPerimetro().ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("DIAGONAL = " + retangulo.CalcularDiagonal().ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_POO_05_Retangulo
+{
+    public class Retangulo
+    {
+        public double Largura { get; set; }
+        public double Altura { get; set; }
+
+        public double CalcularArea() => Largura * Altura;
+
+        public double CalcularPerimetro() => 2 * (Largura + Altura);
+
+        public double CalcularDiagonal() => Math.Sqrt((Largura * Largura) + (Altura * Altura));
+    }
+}
